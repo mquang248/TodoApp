651f58cf4c0d49980bb025a1170900c3463c1c4b
mquang248 <mquang@gmail.com>
2025-09-28 22:20:57 +0700

    Update

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   extends: ['react-app', 'react-app/jest'],
   rules: {
-    'no-unused-vars': 'warn',
-    'react-hooks/exhaustive-deps': 'warn'
+    'no-unused-vars': 'off',
+    'react-hooks/exhaustive-deps': 'off'
   }
 };
